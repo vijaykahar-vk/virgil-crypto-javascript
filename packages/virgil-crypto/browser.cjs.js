5820bde02fc15fc726a7858aefbf7e00a80e70f4
vijaykahar-vk <121007766+vijaykahar-vk@users.noreply.github.com>
2024-01-23 15:58:56 +0530

    Update browser.cjs.js
    Remove unwanted option condition

--- packages/virgil-crypto/browser.cjs.js
+++ packages/virgil-crypto/browser.cjs.js
@@ -1,10 +1,5 @@
 const { initCrypto } = require('./dist/browser.cjs');
-const foundationWasm = require('./dist/libfoundation.browser.wasm');
-
-const defaultOptions = {
-  foundation: [{ locateFile: () => foundationWasm }],
-};
 
 module.exports = require('./dist/browser.cjs');
 
-module.exports.initCrypto = options => initCrypto(options || defaultOptions);
+module.exports.initCrypto = options => initCrypto(options);
